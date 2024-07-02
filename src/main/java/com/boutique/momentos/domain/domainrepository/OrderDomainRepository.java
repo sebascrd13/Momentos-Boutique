945bb8a835fc041dd835800a78743895948688b2
Jonathan D. Barrantes Segovia <scorderoestrada@gmail.com>
2024-07-01 23:30:03 -0600

    Modulo Productos, Categorias y Pedidos: Se quitan especificaciones sobre si metodos son para admin o usuario

--- src/main/java/com/boutique/momentos/domain/domainrepository/OrderDomainRepository.java
+++ src/main/java/com/boutique/momentos/domain/domainrepository/OrderDomainRepository.java
@@ -6,11 +6,11 @@ import java.util.Optional;
 import com.boutique.momentos.domain.domainentity.OrderDomain;
 
 public interface OrderDomainRepository {
-    List<OrderDomain> getAllOrders();  // Obtener todos los pedidos como admin
-    Optional<OrderDomain> getOrderById(int orderId);  // Obtener un pedido por ID como admin
-    List<OrderDomain> getOrdersByUserId(int userId);  // Obtener pedidos por ID de usuario como admin
-    OrderDomain createOrder(OrderDomain order);  // Realizar un pedido como usuario
-    String getOrderStatus(int orderId, int userId);  // Obtener estado de un pedido como usuario
-    void updateOrderStatus(int orderId, String newStatus);  // Actualizar estado de un pedido como admin
-    void deleteOrder(int orderId);  // Eliminar un pedido como admin
+    List<OrderDomain> getAllOrders();
+    Optional<OrderDomain> getOrderById(int orderId);
+    List<OrderDomain> getOrdersByUserId(int userId);
+    OrderDomain createOrder(OrderDomain order);
+    String getOrderStatus(int orderId, int userId);
+    void updateOrderStatus(int orderId, String newStatus);
+    void deleteOrder(int orderId);
 }
